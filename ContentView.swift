a15180a5498bafaa3bf6bea2f145856ae800a7e2
jkyla <52054379+jkyla@users.noreply.github.com>
2019-09-29 18:01:35 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Jason Howard Kendall on 9/27/19.
+//  Copyright © 2019 JHK. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) { () ->
+                Alert in
+                return Alert(title: Text("Hello there!"),
+                             message: Text("This is my first app pop-up"), dismissButton: .default(Text("Awesome!")))
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
